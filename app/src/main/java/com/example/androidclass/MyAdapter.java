54ed68205f0251841ccce5354a7dc9cb7a7a19cd
ytt-fish <754943082@qq.com>
2020-05-07 14:35:50 +0800

    自定义适配器Adapter

---
+++ app/src/main/java/com/example/androidclass/MyAdapter.java
@@ -0,0 +1,49 @@
+package com.example.androidclass;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyAdapter extends ArrayAdapter {
+
+    private static final String TAG = "MyAdapter";
+
+    public MyAdapter(@NonNull Context context, int resource, ArrayList<HashMap<String, String>> list) {
+        super(context, resource, list);//上下文，布局，数据
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {//view行，parent 控件
+        View itemView;
+        itemView = convertView;
+        if (itemView == null) {
+            //获得对象，是否覆盖根布局
+            itemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        //获得当前位置的对象
+        Map<String, String> map = (Map<String, String>) getItem(position);
+        TextView title = itemView.findViewById(R.id.itemTitle);
+        TextView detail = itemView.findViewById(R.id.itemDetail);
+
+        //每一行数据都是一个map
+        title.setText("Title；"+map.get("ItemTitle"));
+        title.setText("detail"+map.get("ItemDetail"));
+
+        //返回的数据用于填充行的数据
+        return  itemView;
+    }
+
+
+}
--- app/src/main/java/com/example/androidclass/MyList2Activity.java
+++ app/src/main/java/com/example/androidclass/MyList2Activity.java
@@ -1,10 +1,9 @@
 package com.example.androidclass;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Adapter;
 import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
@@ -20,7 +19,9 @@ public class MyList2Activity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initListView();
-        this.setListAdapter(listItemAdapter);
+
+        MyAdapter myAdapter=new MyAdapter(this,R.layout.list_item,listItems);
+        this.setListAdapter(myAdapter);
     }
 
     private  void initListView(){
