701c81e6de3749957509d7d291ade7b823f916b1
ytt-fish <754943082@qq.com>
2020-05-14 15:02:52 +0800

    GridView的使用

--- app/src/main/java/com/example/androidclass/MyLitst.java
+++ app/src/main/java/com/example/androidclass/MyLitst.java
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyLitst extends AppCompatActivity {
+    List<String> data=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_litst);
 
-        ListView listView=findViewById(R.id.mylist);
-        String data[]={"11","222"};
-
+        GridView listView=findViewById(R.id.mylist);
+        for(int i=0;i<10;i++){
+            data.add("item"+i);
+        }
 
         ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
         listView.setAdapter(adapter);
